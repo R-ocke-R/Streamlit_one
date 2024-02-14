942934e685b6b1740d89440a9ad69da732d7f8bc
Manu S <sharamamnu2727@gmail.com>
2024-02-14 22:54:36 +0530

    New Feature-> button

--- main.py
+++ main.py
@@ -5,22 +5,33 @@ st.write(" \n")
 
 
 st.subheader('Subheader', divider='rainbow')
-st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
+# st.subheader('_Streamlit_ is :blue[cool] :sunglasses:')
 
 
 col1, col2 = st.columns(2)
-col1.write(' :red[Column 1]')
-col2.write(' :red[Column 2]')
+# col1.write(' :red[Column 1]')
+# col2.write(' :red[Column 2]')
 
 # Three columns with different widths
 col1, col2, col3 = st.columns([3,1,1])
 # col1 is wider
 
-# Using 'with' notation:
-with col1: 
+st.subheader('Button Feature', divider='rainbow')
+
+if st.button('Cat'):
+    st.write("Cat")
     st.image('cat.gif')
-with col2:
+else:
+    st.write("Dog")
     st.image('dog.gif')
 
-st.write("\n \n ##### Here is a video of kanye west")
-st.video('kanye.mp4')
+# Using 'with' notation:
+# with col1: 
+#     st.image('cat.gif')
+# with col2:
+#     st.image('dog.gif')
+
+# st.write("\n \n ##### Here is a video of kanye west")
+# st.video('kanye.mp4')
+    
+
