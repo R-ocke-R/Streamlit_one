8af2b3d7a227a3fb97299d76699f2d3a59f6bc05
Manu S <sharamamnu2727@gmail.com>
2024-03-10 15:57:50 +0530

    Changed from a single company chart, to a page that can show the price of SP500 companies based on the user's input

--- project_portfolio/pages/2_yFinance.py
+++ project_portfolio/pages/2_yFinance.py
@@ -1,27 +1,27 @@
 import yfinance as yf
 import streamlit as st
+import pandas as pd
 
-st.write("""
-# Simple Stock Price App
+url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+sp_list = pd.read_html(url) 
+# this brings back all the tables on the page and convert them to pandas dataframe.
+sp_list_500 = sp_list[0]
 
-Shown are the stock **closing price** and ***volume*** of Google!
+ticker_list = list(sp_list_500['Symbol'].values)
+name_list = list(sp_list_500['Security'].values)
+st.write("Choose one of the SP500 company")
 
-""")
+choice = st.selectbox("here", options = name_list)
+selected_index = name_list.index(choice)
+corresponding_ticker = ticker_list[selected_index]
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+
+tickerData = yf.Ticker(corresponding_ticker)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2020-5-31', end='2024-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.write("""
 ## Closing Price
 """)
 st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
